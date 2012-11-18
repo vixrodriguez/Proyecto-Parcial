2e322e64c9c9a3ffabd8ee5f169a15dc640c9c74
Victor Rodriguez <vix206@gmail.com>
2012-11-18 12:44:47 -0500

    Se removio los Tabs y se pusieron en el boton de MENU. Prototipo de creacion de la base de datos SQLite y cambio en la ventana de Registro(no abrir este activity).

---
+++ DriveSafe/src/com/app/drivesafe/Handler_sqlite.java
@@ -0,0 +1,119 @@
+package com.app.drivesafe;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;		//Crea la base de datos
+import android.database.sqlite.SQLiteOpenHelper;	//Manipula la base de datos
+
+import static android.provider.BaseColumns._ID;
+
+public class Handler_sqlite extends SQLiteOpenHelper{
+
+	public Handler_sqlite(Context context) {
+		super(context, "dbDS", null, 1);
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * Crea la base de datos desde que inicia la App
+	 */
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		
+		/*Se crea la tabla donde se almacenara los datos de las infracciones
+		cometidas por el conductor*/
+		
+		String query = "CREATE TABLE Conductor(" +
+		_ID + "INTEGER PRIMARY KEY AUTOINCREMENT," +
+		"velocidad NUMERIC," + 
+		"fecha TEXT," +
+		"horaActual TEXT," +
+		"lugar TEXT" +
+		")";
+		
+		db.execSQL(query); //Ejecuta la sentencia de creacion de la tabla
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * Metodo que permite la insercion de registros en la base de datos
+	 * @param velocidad Velocidad actual que lleva el conductor
+	 * @param fecha Fecha que se cometio la infraccion
+	 * @param horaActual Hora en que se produjo la infraccion
+	 * @param lugar Lugar que se cometio la infraccion
+	 */
+	public void ingresarRegistro
+	(double velocidad, String fecha, String horaActual, String lugar )
+	{
+		//Se instancia un contenedor de valores para el registro de los datos
+		ContentValues valores = new ContentValues();
+		
+		//Agregando los datos obtenidos con sus respectivos atributos de la DB
+		valores.put("velocidad", velocidad);
+		valores.put("fecha", fecha);
+		valores.put("horaActual", horaActual);
+		valores.put("lugar", lugar);
+		
+		//Agrega el conjunto de valores a la base de datos
+		this.getWritableDatabase().insert("Conductor", null, valores);
+	}
+	
+	/**
+	 * Metodo que realiza la consulta de toda la tabla de la BBDD
+	 */
+	@SuppressWarnings("null")
+	public String[] consultarRegistro()
+	{
+		int id, vel, f, h, l, indice=0;
+		String registro[] = null, temp; //Referencia que apuntara al registro actual que se encuentre
+		
+		//String que especifica que columnas deseo obtener para el SELECT
+		String columnas[] = {_ID,"velocidad","fecha","horaActual","lugar"};
+		
+		//Obtengo el curso de la tabla que contiene el query solicitado
+		Cursor c = this.getReadableDatabase().query("Conductor",columnas,null,null,null,null,null);
+		
+		//Obtengo el indice de cada uno de los registros de la tabla
+		id = c.getColumnIndex(_ID);
+		vel = c.getColumnIndex("velocidad");
+		f = c.getColumnIndex("fecha");
+		h = c.getColumnIndex("horaActual");
+		l = c.getColumnIndex("l");
+		
+		//Mientras tenga filas, se sigue desplazando el curso
+		//y obteniendo los datos
+		for( c.moveToFirst(); !c.isAfterLast(); c.moveToNext())
+		{
+			temp = "Id: "+ c.getString(id) + "\n"+
+					 "Velocidad: " + c.getString(vel) + "\n" +
+					 "Fecha: " + c.getString(f) + "\n" +
+					 "horaActual: " + c.getString(h) + "\n" +
+					 "Lugar: " + c.getString(l) + "\n\n\n";
+			registro[indice] = temp;
+		}
+		return registro;
+	}
+	/**
+	 * Metodo que permite abrir la base de datos
+	 */
+	public void abrir()
+	{
+		this.getWritableDatabase();
+	}
+	
+	/**
+	 * Metodo que permite cerrar la base de datos
+	 */
+	public void cerrar()
+	{
+		this.close();
+	}
+
+}
--- DriveSafe/src/com/app/drivesafe/RegistroActivity.java
+++ DriveSafe/src/com/app/drivesafe/RegistroActivity.java
@@ -1,12 +1,20 @@
 package com.app.drivesafe;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class RegistroActivity extends Activity{
 
+	Handler_sqlite handler;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -23,5 +31,71 @@ public class RegistroActivity extends Activity{
 		(this, R.array.lista, android.R.layout.simple_spinner_item);
 		
 		sp.setAdapter(adp);
+		
+		handler = new Handler_sqlite(this); //Aqui instancia la base de datos
+	}
+	
+	
+	
+	@SuppressWarnings("null")
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		
+		Handler_sqlite handler = null;
+		handler.abrir(); //Abro la base de datos
+		
+		ListView list = (ListView) findViewById(R.id.listaRegistro);
+		this.mostrarListaRegistro(list);
+		handler.cerrar();//Cierro la base de datos
+	}
+
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    //getMenuInflater().inflate(R.menu.activity_velocidad, menu);
+	  	crearMenuOpciones(menu);
+	    return true;
+	}
+	    
+	public void crearMenuOpciones(Menu menuOpciones)
+	{
+	 	//1.Creo una instancia de menu
+		//group, id, orden 
+	   	MenuItem menu1 = menuOpciones.add(0,0,0,"Velocidad");
+	   	MenuItem menu2 = menuOpciones.add(0,1,1,"Registro");
+	   	MenuItem menu3 = menuOpciones.add(0,2,2,"Ajustes");
+	   	
+	   	//2. seteo una tecla de atajo
+	   	menu1.setAlphabeticShortcut('V'); //tecla de acceso
+	   	menu2.setAlphabeticShortcut('R');
+	   	menu3.setAlphabeticShortcut('A');
+	   	
+	   	//3. Seteo Iconos
+	   	menu1.setIcon(R.drawable.ico_velocimetro);
+	   	menu2.setIcon(R.drawable.ico_registro);
+	   	menu3.setIcon(R.drawable.ico_ajustes);
+	   	
+	   	menu1.setIntent(new Intent(this, VelocidadActivity.class));
+	   	menu2.setIntent(new Intent(this, RegistroActivity.class));
+	   	menu3.setIntent(new Intent(this, AjustesActivity.class));
+	}
+	
+	public void mostrarListaRegistro(ListView list)
+	{
+		int i;
+		String registro[] = handler.consultarRegistro();
+		
+		TextView dato;
+
+		for(i=0; i<registro.length; i++)
+		{
+			dato = new TextView(null);
+			dato.setText(registro[i]);
+			
+			list.addView(dato);
+		}
 	}
 }
